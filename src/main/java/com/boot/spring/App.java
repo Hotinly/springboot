c7828eba8628c2c8eb67a7b297a142088898e8ad
Chunlei <hclhubj@cn.ibm.com>
2017-05-17 15:49:47 +0800

    WAR deployment.

--- src/main/java/com/boot/spring/App.java
+++ src/main/java/com/boot/spring/App.java
@@ -2,13 +2,21 @@ package com.boot.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
  * App entrance!
  */
 @SpringBootApplication
-public class App {
+public class App extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
+	}
+
+	//Why I don't Override this, the war file runs well too ???
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return super.configure(builder);
 	}
 }
