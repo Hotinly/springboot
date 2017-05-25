601a3132374ec063902a1f9d38000ef882973cd2
Chunlei <hclhubj@cn.ibm.com>
2017-05-25 17:46:12 +0800

    File upload(Local)

---
+++ src/main/java/com/boot/spring/config/FileUploadConfig.java
@@ -0,0 +1,17 @@
+package com.boot.spring.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FileUploadConfig {
+
+//	// properties文件中的配置的处理，见： MultipartProperties
+//	@Bean
+//	public MultipartConfigElement multipartConfigElement() {
+//		MultipartConfigFactory factory = new MultipartConfigFactory();
+//		// factory.setLocation("D:\\001"); // 上传路径问题
+//		factory.setMaxFileSize("128KB"); // 单文件大小
+//		factory.setMaxRequestSize("128KB"); // 总上传数据大小
+//		return factory.createMultipartConfig();
+//	}
+}
